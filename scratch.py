1afbccaf7bcb705af8fcc8fe2d4ec26fa54c6333
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-10-07 20:23:22 +0200

    stuffs

--- scratch.py
+++ scratch.py
@@ -1,9 +1,29 @@
+from concurrent.futures import ThreadPoolExecutor
+import itertools
 import time
 
 from rich import print
 
 from repo import get_time
 
+
+def m_2(n: int):
+    return pow(n,2)
+
+
+def main():
+    thread_it = True
+    a: list[int] = list(range(1000))
+    b = [] if thread_it else [m_2(i) for i in a]
+    if thread_it:
+        with ThreadPoolExecutor() as executor:
+            b.extend([i for i in executor.map(m_2, a)])
+    print(f"{b=}")
+
+
 if __name__ == "__main__":
-    a = [i for i in range(100)]
-    print(a)
+    t0 = time.time()
+    main()
+    t1 = time.time()
+    t_diff = t1 - t0
+    print(f"{t_diff=}")
