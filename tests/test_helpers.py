f5756e6594a56b7b37fa50ed48c67ccb99aa6709
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-10-09 23:10:31 +0200

    test singleton

---
+++ tests/test_helpers.py
@@ -0,0 +1,22 @@
+from pydoc import classname
+
+from helpers import Singleton
+
+
+class TestClass(metaclass=Singleton):
+    x = 0
+
+
+def test_singleton_instance():
+    instance1 = TestClass()
+    instance2 = TestClass()
+
+    assert instance1 is instance2
+
+
+def test_singleton_variable():
+    instance1 = TestClass()
+    instance2 = TestClass()
+
+    instance1.x = 1
+    assert instance1.x == instance2.x
