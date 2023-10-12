1d4f868b32a61747925f7726e139902191d7a909
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-10-12 15:31:43 +0200

    factor it

---
+++ base/factorytools.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+
+class Product(ABC):
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+class Creator(ABC):
+    @abstractmethod
+    def factory_method(self):
+        pass
+
+    def some_operation(self) -> str:
+        product = self.factory_method()
+
+        return f"Creator: The same creator's code has just worked with {product.operation()}"
+
+
+class ConcreteProduct1(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct1}"
+
+
+class ConcreteProduct2(Product):
+    def operation(self) -> str:
+        return "{Result of the ConcreteProduct2}"
+
+
+class ConcreteCreator1(Creator):
+    def factory_method(self) -> Product:
+        return ConcreteProduct1()
+
+
+class ConcreteCreator2(Creator):
+    def factory_method(self) -> Product:
+        return ConcreteProduct2()
+
+
+def client_code(creator: Creator) -> None:
+    print(
+        f"Client: I'm not aware of the creator's class, but it still works.\n"
+        f"{creator.some_operation()}",
+        end="",
+    )
+
+
+if __name__ == "__main__":
+    print("App: Launched with the ConcreteCreator1.")
+    client_code(ConcreteCreator1())
+    print("\n")
+    print("App: Launched with the ConcreteCreator2.")
+    client_code(ConcreteCreator2())
