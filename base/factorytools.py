37de435ca0068de5f281d5e063166ffc30e28943
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-10-12 15:36:45 +0200

    produce it

--- base/factorytools.py
+++ base/factorytools.py
@@ -9,12 +9,11 @@ class Product(ABC):
 
 class Creator(ABC):
     @abstractmethod
-    def factory_method(self):
+    def factory_method(self) -> Product:
         pass
 
     def some_operation(self) -> str:
         product = self.factory_method()
-
         return f"Creator: The same creator's code has just worked with {product.operation()}"
 
 
