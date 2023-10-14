b1ff622720bd8f72ec7e08da5f4984c326952510
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-10-14 17:09:34 +0200

    that's good enough

--- base/consts.py
+++ base/consts.py
@@ -1,4 +1,10 @@
-KEY = "1bd171c9e4c7831ace3c358d9c93ba3cc0304728d90a243830428a2c0dabfead"
-SECRET = "787a28958d7d9ce9e189527b2b4d8a4b608c8474d6447a23c3c88d9ff81c1269"
-BASE_URL = "https://testnet.binancefuture.com"
-STREAM_URL = "wss://stream.binancefuture.com"
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KEY = os.getenv("KEY")
+SECRET = os.getenv("SECRET")
+BASE_URL = os.getenv("BASE_URL")
+STREAM_URL = os.getenv("STREAM_URL")
