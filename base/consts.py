a19da71dd570a1c20bfae7ac44698cdd0af31851
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-10-18 12:44:09 +0200

    validate env vars

--- base/consts.py
+++ base/consts.py
@@ -4,7 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-KEY = os.getenv("KEY")
-SECRET = os.getenv("SECRET")
-BASE_URL = os.getenv("BASE_URL")
-STREAM_URL = os.getenv("STREAM_URL")
+
+def get_env_var(key: str) -> str:
+    if result := os.getenv(key):
+        return result
+    raise KeyError(f"Environment variable {key} is not set")
+
+
+KEY = get_env_var("KEY")
+SECRET = get_env_var("SECRET")
+BASE_URL = get_env_var("BASE_URL")
+STREAM_URL = get_env_var("STREAM_URL")
