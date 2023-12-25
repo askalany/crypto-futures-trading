98345a3a7ad1b32be28f1e03e7a436649d81f48f
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-12-25 15:40:58 +0100

    optimize fileutils

--- utils/fileutils.py
+++ utils/fileutils.py
@@ -1,11 +1,14 @@
 import json
+from typing import Optional
+
 from base.models.FileInput import FileInput
 
 
-def get_inputs_from_file(
-    file_name: str = "my_trading.json",
-) -> FileInput:
-    with open(file=file_name, mode="r", encoding="utf-8-sig") as f:
-        read = f.read()
-        data = json.loads(read)
-        return FileInput(**data)
+def get_inputs_from_file(file_name: str = "my_trading.json") -> Optional[FileInput]:
+    try:
+        with open(file_name, "r", encoding="utf-8-sig") as f:
+            data = json.load(f)
+            return FileInput(**data) if data else None
+    except (FileNotFoundError, json.JSONDecodeError, TypeError) as e:
+        print(f"Error reading or decoding the file {file_name}: {e}")
+        return None
