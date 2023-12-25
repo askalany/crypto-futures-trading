111ea31058916ecbbdf1b81c43d1f9cebe4f6b05
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2023-12-25 15:43:58 +0100

    optimize listutils

--- utils/listutils.py
+++ utils/listutils.py
@@ -1,15 +1,15 @@
 from itertools import islice
-from typing import Any, Generator
+from typing import Any
+from typing import Generator
 
 
-def batched(iterable, n) -> Generator[tuple[Any, ...], Any, None]:
+def batched(iterable, n) -> Generator[list[Any], None, None]:
     if n < 1:
         raise ValueError("n must be at least one")
     it = iter(iterable)
-    while batch := tuple(islice(it, n)):
+    while batch := list(islice(it, n)):
         yield batch
 
 
 def batched_lists(iterable, n) -> list[list[Any]]:
-    b = batched(iterable=iterable, n=n)
-    return [list(i) for i in b]
+    return list(batched(iterable=iterable, n=n))
